361b5afdbfc56f4558af1b36e26d669e0f214f5b
Rafael Buchbinder <rafi@rbk.io>
2023-10-27 18:12:13 +0300

    tighterror: crates.io placeholder

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! An error representation framework with compact error types and a code
+//! generator.
+//!
+//! `tighterror` is under development. This version is a crates.io placeholder.
+
+#![no_std]
+#![deny(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
