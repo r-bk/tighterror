cbe7ec595ba2a95cf33d48250ced2a4d51dd9800
Rafael Buchbinder <rafi@rbk.io>
2023-12-09 17:19:43 +0200

    tighterror: add the accompanying crates skeleton
    Note that Cargo.lock as removed from .gitignore, as it is required for
    the `cargo-tighterror` crate, which is a binary.

---
+++ crates/cargo-tighterror/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("cargo-tighterror, under construction");
+}
---
+++ crates/tighterror-build/src/lib.rs
@@ -0,0 +1,5 @@
+//! `tighterror-build` is a library, part of the `tighterror` framework, that
+//! implements Rust code generation from a markup language specification file.
+
+#![deny(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
