f7f0f77df592c8f4fc66cdb41b057e405262d581
Rafael Buchbinder <rafi@rbk.io>
2023-12-23 08:57:27 +0200

    tb: move `*_into_result` tests to `minimal` test
    In preparation for testing the negative flow in `implicit_category`.

--- crates/tighterror-build/tests/implicit_category/src/lib.rs
+++ crates/tighterror-build/tests/implicit_category/src/lib.rs
@@ -7,18 +7,3 @@
 pub mod errors {
     include!(concat!(env!("OUT_DIR"), "/errors.rs"));
 }
-
-#[cfg(test)]
-mod tests {
-    use super::errors::*;
-
-    #[test]
-    fn test_err_into_result() {
-        let _res: Result<(), Error> = Error::from(codes::BAD_INPUT).into();
-    }
-
-    #[test]
-    fn test_err_code_into_result() {
-        let _res: Result<(), Error> = codes::BAD_INPUT.into();
-    }
-}
--- crates/tighterror-build/tests/minimal/src/lib.rs
+++ crates/tighterror-build/tests/minimal/src/lib.rs
@@ -5,3 +5,18 @@
 pub mod errors {
     include!(concat!(env!("OUT_DIR"), "/errors.rs"));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::errors::*;
+
+    #[test]
+    fn test_err_into_result() {
+        let _res: Result<(), Error> = Error::from(codes::BAD_FILE).into();
+    }
+
+    #[test]
+    fn test_err_code_into_result() {
+        let _res: Result<(), Error> = codes::BAD_FILE.into();
+    }
+}
