537d65af7db5366893fdd93bec55e90ffedaf44d
Rafael Buchbinder <rafi@rbk.io>
2024-01-05 09:06:58 +0200

    tb: fix clippy warnings in `compilation` test

--- crates/tighterror-build/tests/compilation/tests/tests.rs
+++ crates/tighterror-build/tests/compilation/tests/tests.rs
@@ -3,10 +3,12 @@ extern crate compiletest_rs as compiletest;
 use std::path::PathBuf;
 
 fn run_mode(mode: &'static str) {
-    let mut config = compiletest::Config::default();
+    let mut config = compiletest::Config {
+        mode: mode.parse().expect("Invalid mode"),
+        src_base: PathBuf::from(format!("tests/{}", mode)),
+        ..Default::default()
+    };
 
-    config.mode = mode.parse().expect("Invalid mode");
-    config.src_base = PathBuf::from(format!("tests/{}", mode));
     config.link_deps(); // Populate config.target_rustcflags with dependencies on the path
     config.clean_rmeta(); // If your tests import the parent crate, this helps with E0464
 
