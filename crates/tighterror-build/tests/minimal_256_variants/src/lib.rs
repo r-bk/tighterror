1c989368a9a8314b12754ed66ba8c7487db1c4f0
Rafael Buchbinder <rafi@rbk.io>
2024-02-09 11:52:19 +0200

    tb: add a test for all-bits-as-variant case
    This commit adds a test for the case when all bits of a tighterror are
    variant bits. This is possible when there is a single category (hence no
    category bits), and the number of variants equals the max number
    supported by the underlying Rust type (in this case u8).

---
+++ crates/tighterror-build/tests/minimal_256_variants/src/lib.rs
@@ -0,0 +1,21 @@
+//! This is a test crate to check `tighterror.yaml`
+//! with minimal configuration.
+
+#![deny(warnings)]
+
+/// Crate errors.
+pub mod errors {
+    include!(concat!(env!("OUT_DIR"), "/errors.rs"));
+
+    #[cfg(test)]
+    mod more_tests {
+        use super::*;
+
+        #[test]
+        fn test_constants() {
+            assert_eq!(_p::KIND_BITS, u8::BITS as usize);
+            assert_eq!(_p::CAT_BITS, 0);
+            assert_eq!(_p::VAR_MASK, u8::MAX);
+        }
+    }
+}
