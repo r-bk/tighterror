1ea9cce4835244b3c00421960f41c18193083748
Rafael Buchbinder <rafi@rbk.io>
2024-03-15 09:14:22 +0200

    tb: add `no_std` test

---
+++ crates/tighterror-build/tests/minimal_no_std/src/lib.rs
@@ -0,0 +1,25 @@
+//! This is a test crate to check `tighterror.yaml`
+//! with minimal configuration in no_std environment.
+
+#![deny(warnings)]
+#![no_std]
+
+/// Crate errors.
+pub mod errors {
+    include!(concat!(env!("OUT_DIR"), "/errors.rs"));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::errors::*;
+
+    #[test]
+    fn test_result_from_err() {
+        let _res: Result<(), Error> = Error::from(kinds::BAD_FILE).into();
+    }
+
+    #[test]
+    fn test_result_from_err_kind() {
+        let _res: Result<(), Error> = kinds::BAD_FILE.into();
+    }
+}
